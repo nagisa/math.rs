8c3f5544f76e82ad0c02789d5efb729fe03f26e7
Simonas Kazlauskas <git@kazlauskas.me>
2015-01-06 23:19:34 +0200

    Implement lroundf

--- src/rounding.rs
+++ src/rounding.rs
@@ -1,26 +1,9 @@
 use core::mem::transmute;
+use core::mem::size_of;
 use libc::types::os::arch::c99;
 use libc::types::os::arch::c95;
-
-#[no_mangle]
-pub extern fn lround(i : f64) -> c95::c_long {
-    0
-}
-
-#[no_mangle]
-pub extern fn lroundf(i : f32) -> c95::c_long {
-    0
-}
-
-#[no_mangle]
-pub extern fn llround(i : f64) -> c99::c_longlong {
-    0
-}
-
-#[no_mangle]
-pub extern fn llroundf(i : f32) -> c99::c_longlong {
-    0
-}
+use core::option::Option;
+use core::option::Option::{Some,None};
 
 /// Round the number away from zero.
 #[no_mangle]
@@ -205,6 +188,65 @@ pub extern fn truncf(i : f32) -> f32 {
     unsafe { transmute(bits) }
 }
 
+/// Round number away from zero.
+///
+/// If the number is ∞, NaN or doesn’t fit into the result value, None is returned.
+#[inline]
+fn lroundf_(i : f32) -> Option<c95::c_long> {
+    let mut bits : u32 = unsafe { transmute(i) };
+    let exp = (((bits as i32) >> 23) & 0xff) - 0x7f;
+    let sign : c95::c_long;
+    let result : c95::c_long;
+
+    // Calculate the sign
+    if bits & 0x8000_0000 != 0 {
+        sign = -1
+    } else {
+        sign = 1
+    }
+
+    // Calculate the result
+    bits = (bits & 0x007f_ffff) | 0x0080_0000;
+    if exp >= (8 * size_of::<c95::c_long>() - 1) as i32 {
+        // The number is too large for the output type
+        return None
+    } else if exp == -1 {
+        return Some(sign);
+    } else if exp < -1 {
+        return Some(0);
+    } else if exp < 23 {
+        bits += 0x0040_0000 >> (exp as uint);
+        result = (bits >> ((23 - exp) as uint)) as c95::c_long;
+    } else {
+        result = (bits << ((exp - 23) as uint)) as c95::c_long;
+    }
+    Some(sign * result)
+}
+
+#[no_mangle]
+pub extern fn lround(i : f64) -> c95::c_long {
+    0
+}
+
+/// Round number away from zero.
+///
+/// If the number is ∞, NaN or doesn’t fit into the result value, arbitrary value may be returned.
+#[no_mangle]
+pub extern fn lroundf(i : f32) -> c95::c_long {
+    lroundf_(i).unwrap_or(0)
+}
+
+#[no_mangle]
+pub extern fn llround(i : f64) -> c99::c_longlong {
+    0
+}
+
+#[no_mangle]
+pub extern fn llroundf(i : f32) -> c99::c_longlong {
+    0
+}
+
+
 #[test]
 fn round_f64() {
     use core::f64;
@@ -235,6 +277,21 @@ fn round_f32() {
     assert_eq!(roundf(123456789.500001), 123456790.0);
     assert_eq!(roundf(f32::NEG_INFINITY), f32::NEG_INFINITY);
     assert!(roundf(f32::NAN).is_nan());
+}
+
+#[test]
+fn lround_f32() {
+    use core::f32;
+    use core::num::Float;
+    assert_eq!(lroundf(0.0), 0);
+    assert_eq!(lroundf(1.0), 1);
+    assert_eq!(lroundf(1.6), 2);
+    assert_eq!(lroundf(-1.5), -2);
+    assert_eq!(lroundf(-1.4), -1);
+    assert_eq!(lroundf(-0.01), 0);
+    // Float can’t hold this value exactly, but it still fits into integer.
+    // Allow to be off from expected result by at most 1
+    assert!(lroundf(2147483647.0) - 2147483646 <= 2);
 }
 
 #[test]
