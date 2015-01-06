52db68c76d8432f639c9a74b4a78a8d082e7bae2
Simonas Kazlauskas <git@kazlauskas.me>
2015-01-06 20:39:24 +0200

    Add roundf

--- src/rounding.rs
+++ src/rounding.rs
@@ -27,7 +27,6 @@ pub fn llroundf(i : f32) -> c95::c_long {
 pub fn round(i : f64) -> f64 {
     // Port of glibc libm implementation.
     let mut bits : u64 = unsafe { transmute(i) };
-
     let high : u32 = (bits >> 32) as u32;
     let exp : i32 = (((high >> 20) as i32) & 0x7ff) - 0x3ff;
     let low : u32 = bits as u32;
@@ -47,7 +46,8 @@ pub fn round(i : f64) -> f64 {
         // Disrepancy: Original implementation raises overflow here
         bits += 0x0008_0000_0000_0000 >> (exp as uint);
         bits &= (!temp as u64) << 32;
-    } else if exp > 51 { // Infinite or NaN
+    } else if exp > 51 {
+        // Infinite or NaN
         if exp == 0x400 {
             return i + i;
         } else {
@@ -71,7 +71,30 @@ pub fn round(i : f64) -> f64 {
 
 #[no_mangle]
 pub fn roundf(i : f32) -> f32 {
-    0.0
+    let mut bits : u32 = unsafe { transmute(i) };
+    let exp = (((bits as i32) >> 23) & 0xff) - 0x7f;
+    if exp < 0 {
+        bits &= 0x8000_0000;
+        if exp == -1 {
+            bits |= 0x3f80_0000;
+        }
+    } else if exp < 23 {
+        let temp = 0x007f_ffff >> (exp as uint);
+        if bits & temp == 0 {
+            // The input is already integral
+            return i;
+        }
+        bits += 0x0040_0000 >> (exp as uint);
+        bits &= !temp;
+    } else {
+        // Infinite or NaN
+        if exp == 0x80 {
+            return i + i;
+        } else {
+            return i;
+        }
+    }
+    unsafe { transmute(bits) }
 }
 
 #[no_mangle]
@@ -114,8 +137,24 @@ fn round_f64() {
     assert_eq!(round(-1.5f64), -2.0);
     assert_eq!(round(-1.4f64), -1.0);
     assert_eq!(round(-0.01f64), 0.0);
-    assert_eq!(round(f64::NEG_INFINITY), f64::NEG_INFINITY);
     assert_eq!(round(1.7654321E13), 1.7654321E13);
     assert_eq!(round(123456789.500001), 123456790.0);
+    assert_eq!(round(f64::NEG_INFINITY), f64::NEG_INFINITY);
     assert!(round(f64::NAN).is_nan());
+}
+
+#[test]
+fn round_f32() {
+    use core::f32;
+    use core::num::Float;
+    assert_eq!(roundf(0.0f32), 0.0);
+    assert_eq!(roundf(1.0f32), 1.0);
+    assert_eq!(roundf(1.5f32), 2.0);
+    assert_eq!(roundf(-1.5f32), -2.0);
+    assert_eq!(roundf(-1.4f32), -1.0);
+    assert_eq!(roundf(-0.01f32), 0.0);
+    assert_eq!(roundf(1.7654321E13), 1.7654321E13);
+    assert_eq!(roundf(123456789.500001), 123456790.0);
+    assert_eq!(roundf(f32::NEG_INFINITY), f32::NEG_INFINITY);
+    assert!(roundf(f32::NAN).is_nan());
 }
