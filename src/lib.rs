1533d205bc7916b8c9899a3a8bc2653924ae3f60
Simonas Kazlauskas <git@kazlauskas.me>
2015-01-07 17:35:16 +0200

    Readd fdim*

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@
 //!
 //! This is supposed to be linked into Rust binaries on systems where libm is not available or
 //! desirable.
+//!
+//! Note that this library does not provide some of more obscure functionality of libm such as
+//! global state (rounding direction, error reporting, etc).
 
 #![crate_name="math"]
 #![crate_type="rlib"]
@@ -27,17 +30,17 @@ extern crate libc;
 // Reexport everything
 pub use signops::*;
 pub use rounding::*;
+pub use arithmetic::*;
 
 // The modules are split and grouped by functionality, not data type size, architecture or any
-// other arbitrary property:
+// other arbitrary property.
 //
-// Operations on signs (absolute, copysign).
+// Operations on signs (absolute, copysign, …).
 pub mod signops;
-// Rounding operations (round, floor, ceil, truncate).
+// Rounding operations (round, floor, ceil, truncate, …).
 pub mod rounding;
+// Simple arithmetic operations (positive difference, modulo operation, min, max, …).
+pub mod arithmetic;
+
 // Common functionality.
 mod utils;
-
-
-// // Positive difference
-// pub mod dim;
