91d69f4feded68c74b2de2755a1353042314e5b8
Simonas Kazlauskas <git@kazlauskas.me>
2015-01-07 21:36:56 +0200

    ilogb cleanup

--- src/ilogb.rs
+++ src/ilogb.rs
@@ -1,18 +1,18 @@
 use core::intrinsics::{ctlz32, ctlz64};
 // TODO: remove this
-use libc::types::os::arch::c95;
+use libc::c_int;
 
 use utils::{AsBits};
 use utils::{F32_SIGN_MASK, F64_SIGN_MASK};
 
 /// Target and library dependent constant (i.e. might not be compatible with math.h).
-pub const FP_ILOGB0 : c95::c_int = -0x8000_0000;
+pub const FP_ILOGB0 : c_int = -0x8000_0000;
 /// Target and library dependent constant (i.e. might not be compatible with math.h).
-pub const FP_ILOGBNAN : c95::c_int = 0x7FFF_FFFF;
+pub const FP_ILOGBNAN : c_int = 0x7FFF_FFFF;
 
 /// Get exponent of of a 32-bit floating-point value as an int.
 #[no_mangle]
-pub extern fn ilogbf(i: f32) -> c95::c_int {
+pub extern fn ilogbf(i: f32) -> c_int {
     let mut bits = i.as_bits();
     bits &= !F32_SIGN_MASK;
     if bits == 0 {
@@ -23,16 +23,16 @@ pub extern fn ilogbf(i: f32) -> c95::c_int {
         unsafe {
             exp -= ctlz32(bits) - 9;
         }
-        return exp as c95::c_int;
+        return exp as c_int;
     } else if bits < 0x7F80_0000 {
-        return ((bits >> 23) as c95::c_int) - 0x7F;
+        return ((bits >> 23) as c_int) - 0x7F;
     }
     FP_ILOGBNAN
 }
 
 /// Get exponent of of a 64-bit floating-point value.
 #[no_mangle]
-pub extern fn ilogb(i: f64) -> c95::c_int {
+pub extern fn ilogb(i: f64) -> c_int {
     let mut bits = i.as_bits();
     bits &= !F64_SIGN_MASK;
     if bits == 0 {
@@ -43,9 +43,9 @@ pub extern fn ilogb(i: f64) -> c95::c_int {
         unsafe {
             exp -= ctlz64(bits) - 12;
         }
-        return exp as c95::c_int;
+        return exp as c_int;
     } else if bits < 0x7FF0_0000_0000_0000 {
-        return ((bits >> 52) as c95::c_int) - 0x3FF;
+        return ((bits >> 52) as c_int) - 0x3FF;
     }
     FP_ILOGBNAN
 }
