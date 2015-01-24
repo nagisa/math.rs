68bde38602c89e297e7fd258a9b165c227840d9c
Simonas Kazlauskas <git@kazlauskas.me>
2015-01-24 15:06:26 +0200

    Use methods

--- src/acos.rs
+++ src/acos.rs
@@ -1,15 +1,13 @@
 use core::f64::consts::{FRAC_PI_2};
 use core::num::Float;
 
-use asin::{asin};
-
 /// Calculate the arc cosine of an input.
 #[no_mangle]
 pub extern fn acos(i: f64) -> f64 {
     if i > 0.0 {
-        FRAC_PI_2 - asin(i)
+        FRAC_PI_2 - i.asin()
     } else {
-        FRAC_PI_2 + asin(i.abs())
+        FRAC_PI_2 + i.abs().asin()
     }
 }
 
