c31b3de2ca317084b7e736eb9e5bacaccd9ebf85
Simonas Kazlauskas <git@kazlauskas.me>
2015-02-02 11:46:52 +0200

    Expand sqrt tests

--- tests/sqrtlog.rs
+++ tests/sqrtlog.rs
@@ -5,37 +5,43 @@ use std::{f32, f64};
 
 use math::{sqrtf, sqrt};
 use math::{expf, exp};
+use testutils::*;
+
+#[macro_use]
+mod testutils;
 
 #[test]
 fn sqrt_f32() {
-    assert_eq!(sqrtf(0.0), 0.0);
-    assert_eq!(sqrtf(4.0), 2.0);
-    assert_eq!(sqrtf(9.0), 3.0);
-    assert_eq!(sqrtf(10000.0), 100.0);
-    assert_eq!(sqrtf(110.25), 10.5);
-
-    assert_eq!(sqrt(1.4E-45), 3.741657386773942e-23);
-
-    assert_eq!(sqrtf(f32::INFINITY), f32::INFINITY);
-    assert!(sqrtf(-1.0).is_nan());
-    assert!(sqrtf(f32::NAN).is_nan());
-    assert!(sqrtf(f32::NEG_INFINITY).is_nan());
+    assert_feq!(sqrtf(0.0),      0.0,                       0.0,   TEST_ZERO_SIGN);
+    assert_feq!(sqrtf(4.0),      2.0,                       0.0,   0);
+    assert_feq!(sqrtf(9.0),      3.0,                       0.0,   0);
+    assert_feq!(sqrtf(20.25),    4.5,                       0.0,   0);
+    assert_feq!(sqrtf(0.065536), 0.256,                     0.0,   0);
+    assert_feq!(sqrtf(1.4E-45),  3.7416573867739413855E-23, 1E-25, 0);
+    assert_feq!(sqrtf(f32::NAN),          f32::NAN,      0.0, 0);
+    assert_feq!(sqrtf(f32::INFINITY),     f32::INFINITY, 0.0, 0);
+    assert_feq!(sqrtf(-1.0),              f32::NAN,      0.0, 0);
+    assert_feq!(sqrtf(f32::NEG_INFINITY), f32::NAN,      0.0, 0);
+    assert_feq!(sqrtf(f32::MIN_VALUE),    f32::NAN,      0.0, 0);
 }
 
 #[test]
 fn sqrt_f64() {
-    assert_eq!(sqrt(0.0), 0.0);
-    assert_eq!(sqrt(4.0), 2.0);
-    assert_eq!(sqrt(9.0), 3.0);
-    assert_eq!(sqrt(10000.0), 100.0);
-    assert_eq!(sqrt(110.25), 10.5);
+    assert_feq!(sqrt(0.0),      0.0,                       0.0,   TEST_ZERO_SIGN);
+    assert_feq!(sqrt(4.0),      2.0,                       0.0,   0);
+    assert_feq!(sqrt(9.0),      3.0,                       0.0,   0);
+    assert_feq!(sqrt(20.25),    4.5,                       0.0,   0);
+    assert_feq!(sqrt(0.065536), 0.256,                     0.0,   0);
+    assert_feq!(sqrt(1.4E-45),  3.741657386773941385583748732316549301756019807E-23, 1E-35, 0);
 
-    assert_eq!(sqrt(4.94065645841246544176568792868E-324), 2.2227587494850775e-162);
+    assert_feq!(sqrt(4.94065645841246544176568792868E-324), 2.22275874948507748344271341427006E-162,
+                1E-35, 0);
 
-    assert_eq!(sqrt(f64::INFINITY), f64::INFINITY);
-    assert!(sqrt(-1.0).is_nan());
-    assert!(sqrt(f64::NAN).is_nan());
-    assert!(sqrt(f64::NEG_INFINITY).is_nan());
+    assert_feq!(sqrt(f64::NAN),          f64::NAN,      0.0, 0);
+    assert_feq!(sqrt(f64::INFINITY),     f64::INFINITY, 0.0, 0);
+    assert_feq!(sqrt(-1.0),              f64::NAN,      0.0, 0);
+    assert_feq!(sqrt(f64::NEG_INFINITY), f64::NAN,      0.0, 0);
+    assert_feq!(sqrt(f64::MIN_VALUE),    f64::NAN,      0.0, 0);
 }
 
 #[test]
